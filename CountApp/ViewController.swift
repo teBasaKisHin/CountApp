aff892745ef4ce7cef9ab176e8c21d8cc616eb3c
Kawasaki <tebasaki.wejobs@gmail.com>
2017-11-03 12:34:18 +0900

    First Commit of CountApp : iPhoneApp

--- CountApp/ViewController.swift
+++ CountApp/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus() {
+        number += 1
+        label.text = String(number)
+        changeColor()
+    }
+    
+    @IBAction func minus() {
+        number -= 1
+        label.text = String(number)
+        changeColor()
+    }
+    
+    @IBAction func kakeru() {
+        number *= 2
+        label.text = String(number)
+        changeColor()
+    }
+
+    @IBAction func waru() {
+        number /= 2
+        label.text = String(number)
+        changeColor()
+    }
+    
+    @IBAction func changeColor() {
+        if (number >= 1000) {
+            label.textColor = UIColor.red
+        } else if (number >= 100) {
+            label.textColor = UIColor.black
+        }else if(number >= 10) {
+            label.textColor = UIColor.green
+        } else {
+            label.textColor = UIColor.orange
+        }
+    }
 
 }
 
